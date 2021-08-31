930049a3ae5c623ff5b0086cea88268aac44b150
Johan Tibbelin <johan.roos@tibbelin.com>
2021-08-31 23:20:30 +0200

    Added h-scrolling on all lines and DLI

--- src/savefuture.asm
+++ src/savefuture.asm
@@ -41,36 +41,41 @@ start
 ; Display List
 ;
 	.local dl
-	.byte $70
-lms00	.byte $44,a(sm)
-lms01	.byte $44,a(sm+160)
-lms02	.byte $44,a(sm+320)
-lms03	.byte $44,a(sm+480)
-lms04	.byte $44,a(sm+640)
-lms05	.byte $44,a(sm+800)
-lms06	.byte $44,a(sm+960)
-lms07	.byte $44,a(sm+1120)
+	.byte $F0
+lms00	.byte $D4,a(sm)
+lms01	.byte $D4,a(sm+160)
+lms02	.byte $D4,a(sm+320)
+lms03	.byte $D4,a(sm+480)
+lms04	.byte $D4,a(sm+640)
+lms05	.byte $D4,a(sm+800)
+lms06	.byte $D4,a(sm+960)
+lms07	.byte $D4,a(sm+1120)
 lms08	.byte $44,a(sm+1280)
 lms09	.byte $44,a(sm+1440)
 lms10	.byte $44,a(sm+1600)
 lms11	.byte $44,a(sm+1760)
-lms12	.byte $44,a(sm+1920)
-lms13	.byte $44,a(sm+2080)
-lms14	.byte $44,a(sm+2240)
-lms15	.byte $44,a(sm+2400)
-lms16	.byte $44,a(sm+2560)
-lms17	.byte $44,a(sm+2720)
-lms18	.byte $44,a(sm+2880)
-lms19	.byte $44,a(sm+3040)
-lms20	.byte $44,a(sm+3200)
-lms21	.byte $44,a(sm+3360)
-lms22	.byte $44,a(sm+3520)
-lms23	.byte $44,a(sm+3680)
+lms12	.byte $D4,a(sm+1920)
+lms13	.byte $54,a(sm+2080)
+lms14	.byte $54,a(sm+2240)
+lms15	.byte $54,a(sm+2400)
+lms16	.byte $54,a(sm+2560)
+lms17	.byte $54,a(sm+2720)
+lms18	.byte $D4,a(sm+2880)
+lms19	.byte $D4,a(sm+3040)
+lms20	.byte $D4,a(sm+3200)
+lms21	.byte $D4,a(sm+3360)
+lms22	.byte $D4,a(sm+3520)
+lms23	.byte $D4,a(sm+3680)
+txt		.byte $06,a(text)
 		.byte $41,a(dl)
 		
 
 ;
 ; Character sets
 ;
+
+;
+; Screen memory
+
+text	.byte "Test of text"
 	.local sm
-	
