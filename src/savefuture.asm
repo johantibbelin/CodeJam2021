9b6e134c99f8cd68456f7b37b3e084b49cb59d46
Johan Tibbelin <johan.roos@tibbelin.com>
2021-09-01 18:33:56 +0200

    More stuff

--- src/savefuture.asm
+++ src/savefuture.asm
@@ -22,7 +22,18 @@ SETVBV=$e45c
 ;
 
 start
-	jmp start	
+; 	Load display list
+	lda #<dl
+	sta $230
+	lda #>dl
+	sta $231
+	; wide playfield
+	;lda #35
+	;sta $22f
+	lda #0
+	sta HSCRL
+
+stop	jmp stop	
 
 ;
 	run start	
@@ -42,31 +53,31 @@ start
 ;
 	.local dl
 	.byte $F0
-lms00	.byte $D4,a(sm)
-lms01	.byte $D4,a(sm+160)
-lms02	.byte $D4,a(sm+320)
-lms03	.byte $D4,a(sm+480)
-lms04	.byte $D4,a(sm+640)
-lms05	.byte $D4,a(sm+800)
-lms06	.byte $D4,a(sm+960)
-lms07	.byte $D4,a(sm+1120)
-lms08	.byte $44,a(sm+1280)
-lms09	.byte $44,a(sm+1440)
-lms10	.byte $44,a(sm+1600)
-lms11	.byte $44,a(sm+1760)
-lms12	.byte $D4,a(sm+1920)
-lms13	.byte $54,a(sm+2080)
-lms14	.byte $54,a(sm+2240)
-lms15	.byte $54,a(sm+2400)
-lms16	.byte $54,a(sm+2560)
-lms17	.byte $54,a(sm+2720)
-lms18	.byte $D4,a(sm+2880)
-lms19	.byte $D4,a(sm+3040)
-lms20	.byte $D4,a(sm+3200)
-lms21	.byte $D4,a(sm+3360)
-lms22	.byte $D4,a(sm+3520)
-lms23	.byte $D4,a(sm+3680)
-txt		.byte $06,a(text)
+lms00	.byte $45,$00,$40
+lms01	.byte $44,$00,$41
+lms02	.byte $44,$00,$42
+lms03	.byte $44,$00,$43
+lms04	.byte $44,$00,$44
+lms05	.byte $44,$00,$45
+lms06	.byte $44,$00,$46
+lms07	.byte $44,$00,$47
+lms08	.byte $44,$00,$48
+lms09	.byte $44,$00,$49
+lms10	.byte $44,$00,$50
+lms11	.byte $44,$00,$51
+lms12	.byte $44,$00,$52
+lms13	.byte $44,$00,$53
+lms14	.byte $44,$00,$54
+lms15	.byte $44,$00,$55
+lms16	.byte $44,$00,$56
+lms17	.byte $44,$00,$57
+lms18	.byte $44,$00,$58
+lms19	.byte $44,$00,$60
+lms20	.byte $44,$00,$61
+lms21	.byte $44,$00,$62
+lms22	.byte $44,$00,$63
+lms23	.byte $44,$00,$64
+txt		.byte $47,$00,$65
 		.byte $41,a(dl)
 	.endl
 ;
@@ -74,28 +85,29 @@ txt		.byte $06,a(text)
 ;
 	org $4000
 	.local sm
-	.byte "First line"
+	.byte "FIRST LINE"
 	.endl
+	org $4100
+	.byte "Second line"
+	org $6500
+	;.local text	
+text	.byte "Test of text"
 	
-	org *+$3836
-	.local text	
-	.byte "Test of text"
-	.endl
 		
 ;
 ; Character sets
 ;
-	org $5000
+	org $7000
 	.local chset0
 	.endl
-	org $5400
+	org $7400
 	.local chset1
 	.endl
-	org *+$5800
+	org $7800
 	.local chset2
-	org $6200
+	org $8200
 	.endl
 	.local chset3
-	org $6600
+	org $8600
 	.endl
 	
